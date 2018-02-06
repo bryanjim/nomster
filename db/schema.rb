2f13b6ccad24bd403f2bf90d43083094992b2c0c
Bryan Jimenez <bryan@roguedevs.io>
2018-02-06 06:26:53 +0000

    Added login links in navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205210755) do
+ActiveRecord::Schema.define(version: 20180205225045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20180205210755) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "views", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 20180205210755) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_views_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
