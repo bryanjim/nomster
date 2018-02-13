4153d78815bfcbaf1fb143339808c8261add5024
Bryan Jimenez <bryan@roguedevs.io>
2018-02-13 00:45:10 +0000

    Created a comments database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,12 @@
 class Place < ApplicationRecord
+  #relationships
   belongs_to :user
+  has_many :comments
+  
   geocoded_by :address
   after_validation :geocode
+
+  #data validation
   validates :name, :description, presence: true, length: {minimum: 3}
   validates :address, presence: true, length: {minimum: 10}
 
